3a16c4777259e5783fa00d3886d314a042cc4297
PavloButynets <pavlobutynez@gmail.com>
2023-03-29 11:25:32 +0300

    FINAL

---
+++ Service.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "CPoints.h"
+#include <fstream>
+
+template <typename T>
+CPoint<T>* ReadPoint(std::ifstream& fin)
+{
+	char name; fin >> name;
+	T x, y; fin >> x >> y; fin.get();
+	if (name == '_') return new CPoint<T>(x, y);
+	else return new NamedPoint<T>(name, x, y);
+}
