bf307cf16fa71b1e5476819b2d2f470c677b92a4
PavloButynets <pavlobutynez@gmail.com>
2023-03-29 09:17:32 +0300

    Temalate second

--- points.h
+++ points.h
@@ -1,4 +1,6 @@
 #pragma once
+#include<iostream>
+
 template<typename TNum>
 class CPoint
 {
@@ -6,4 +8,21 @@ class CPoint
 public:
 	CPoint(TNum a,TNum b , :x(a), y(b)) {}
 	CPoint() : x(0), y(0){}
+	TNum get(x) const { return x; }
+	TNum get(y) const { return y; }
+	void print_on(std::ostream& os)
+	{
+		os << '(' << x >> ";" << y << ')';
+	}
+	TNum distance(const CPoint& p);
+	CPoint operator+(const CPoint& p) const
+	{
+		return CPoint(this->x + p.x, this->y + p.y);
+
+	}
+	CPoint operator-(const CPoint& p) const
+	{
+		return CPoint(this->x - p.x, this->y - p.y);
+
+	}
 };
